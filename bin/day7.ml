2b03c7d9b5c7c6d45c65eafd8fa099ad6d5cb70a
Paweł Świątkowski <katafrakt@vivaldi.net>
2023-12-07 23:36:37 +0100

    Day 7, part 2

--- bin/day7.ml
+++ bin/day7.ml
@@ -22,11 +22,12 @@ let card_char_to_int ch =
   | 'A' -> 14
   | num -> int_of_string (String.make 1 num)
 
-let calculate_power cards =
-  let non_uniq_cards =
-    count_unique_elements_hashtbl cards |> List.filter (fun (_, x) -> x > 1)
-  in
-  match non_uniq_cards with
+let card_char_to_int_with_jokers ch =
+  let x = card_char_to_int ch in
+  if x == 11 then 0 else x
+
+let tallied_to_power tallied =
+  match tallied with
   | [ (_, 5) ] -> 6
   | [ (_, 4) ] -> 5
   | [ (_, 3); (_, 2) ] -> 4
@@ -36,16 +37,39 @@ let calculate_power cards =
   | [ (_, 2) ] -> 1
   | _ -> 0
 
-let read_hand line =
-  let cards, bid = Advent.String.split_in_two ' ' line in
-  let card_values =
-    cards |> String.to_seq |> List.of_seq |> List.map card_char_to_int
+let calculate_power cards =
+  cards |> count_unique_elements_hashtbl
+  |> List.filter (fun (_, x) -> x > 1)
+  |> tallied_to_power
+
+let calculate_power_with_jokers cards =
+  let find_max vals =
+    List.fold_left (fun acc (_, el) -> if el > acc then el else acc) 0 vals
   in
-  {
-    cards = card_values;
-    bid = int_of_string bid;
-    power = calculate_power card_values;
-  }
+  let add_jokers_to_max (value, num_of_jokers, list) (x, current) =
+    if current == value then
+      let elem = (x, current + num_of_jokers) in
+      (value, 0, list @ [ elem ])
+    else (value, num_of_jokers, list @ [ (x, current) ])
+  in
+  let without_jokers : (int * int) list =
+    cards |> List.filter (fun x -> x != 0) |> count_unique_elements_hashtbl
+  in
+  let num_of_jokers = List.length (List.filter (fun x -> x == 0) cards) in
+  let max_repetitions = find_max without_jokers in
+  let _, _, l =
+    if num_of_jokers == 5 then (1, 2, [ (1, 5) ])
+    else
+      List.fold_left add_jokers_to_max
+        (max_repetitions, num_of_jokers, [])
+        without_jokers
+  in
+  List.filter (fun (_, x) -> x > 1) l |> tallied_to_power
+
+let read_hand fn power_fn line =
+  let cards, bid = Advent.String.split_in_two ' ' line in
+  let card_values = cards |> String.to_seq |> List.of_seq |> List.map fn in
+  { cards = card_values; bid = int_of_string bid; power = power_fn card_values }
 
 let print_hand hand =
   let _ =
@@ -72,7 +96,22 @@ let cmp_hands h1 h2 =
 
 let sort_hands hands = List.sort cmp_hands hands
 
-let run () =
-  file |> Advent.read_lines |> List.map read_hand |> sort_hands
+let part1 () =
+  file |> Advent.read_lines
+  |> List.map (fun x -> read_hand card_char_to_int calculate_power x)
+  |> sort_hands
   |> List.mapi (fun i el -> (i + 1) * el.bid)
   |> List.fold_left ( + ) 0 |> string_of_int |> print_endline
+
+let part2 () =
+  file |> Advent.read_lines
+  |> List.map (fun x ->
+         read_hand card_char_to_int_with_jokers calculate_power_with_jokers x)
+  |> sort_hands
+  |> List.mapi (fun i el -> (i + 1) * el.bid)
+  |> List.fold_left ( + ) 0 |> string_of_int |> print_endline
+
+let run () =
+  part1 ();
+  part2 ();
+  ()
